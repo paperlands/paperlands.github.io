5761ed328e0f027b571588076674bdac6b6e85a2
ks0m1c_dharma <sakiyamuni@sams.ara>
2025-02-24 01:48:48 +0800

    work out of the box in workflows

--- workbox-config.js
+++ workbox-config.js
@@ -9,5 +9,29 @@ module.exports = {
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
-	]
+	],
+	cleanupOutdatedCaches: true,
+	runtimeCaching: [
+		{
+		  urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+		  handler: 'CacheFirst',
+		  options: {
+			cacheName: 'images-cache',
+		  },
+		},
+		{
+		  urlPattern: /\.(?:css|js|webmanifest)$/,
+		  handler: 'NetworkFirst',
+		  options: {
+			cacheName: 'static-cache',
+		  },
+		},
+		{
+		  urlPattern: /\.(?:html)$/,
+		  handler: 'NetworkFirst',
+		  options: {
+			cacheName: 'html-cache',
+		  },
+		},
+	  ],
 };
